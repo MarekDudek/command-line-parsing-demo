8c0664f43c5341aeafd5eef23af378cd7dbbfb7c
Marek Dudek <marek.dudek@gmail.com>
2017-09-10 22:00:39 +0200

    simple example implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import CommandLineOptions
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
-main = someFunc
+main = greet =<< execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+      <> progDesc "Print a greeting for TARGET" 
+      <> header "hello - a test for optparse-applicative" )
+
+
+greet :: Sample -> IO ()
+greet (Sample h False n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+greet _ = return ()
